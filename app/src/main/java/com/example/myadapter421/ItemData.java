7c356be7d124bef03138d76948b26039e1e0fb61
AndreyOvch <amo-rzn@ya.ru>
2020-07-27 00:30:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myadapter421/ItemData.java
@@ -0,0 +1,32 @@
+package com.example.myadapter421;
+
+import android.graphics.drawable.Drawable;
+
+public class ItemData {
+
+    private Drawable image;
+    private String title;
+    private String subtitle;
+
+
+    public ItemData(Drawable image, String title, String subtitle) {
+        this.image = image;
+        this.title = title;
+        this.subtitle = subtitle;
+
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myadapter421/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.myadapter421;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Random random = new Random();
+
+    private ItemsDataAdapter adapter;
+
+    private List<Drawable> images = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        ListView listView = findViewById(R.id.listView);
+        fillImages();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomItemData();
+            }
+        });
+
+
+        adapter = new ItemsDataAdapter(this, null);
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                showItemData(position);
+            }
+        });
+
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                ItemData itemData = adapter.getItem(position);
+                Toast.makeText(MainActivity.this,
+                        "Title: " + itemData.getTitle() + "\n" +
+                                "Долгое нажатие на элемент списка",
+                        Toast.LENGTH_SHORT).show();
+                //adapter.removeItem(position);
+                return true;
+            }
+        });
+    }
+
+    private void fillImages() {
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_report_image));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_add));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_agenda));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_camera));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_call));
+    }
+
+    private void generateRandomItemData() {
+        adapter.addItem(new ItemData(
+                images.get(random.nextInt(images.size())),
+                "Sample" + adapter.getCount(),
+                "Это мой Sample"));
+    }
+
+
+    private void showItemData(int position) {
+        ItemData itemData = adapter.getItem(position);
+        Toast.makeText(MainActivity.this,
+                "Title: " + itemData.getTitle() + "\n" +
+                        "Subtitle: " + itemData.getSubtitle(),
+                Toast.LENGTH_SHORT).show();
+    }
+}
